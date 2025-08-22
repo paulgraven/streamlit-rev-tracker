3d8349b5439fa5c961a51155bd1798cefb7b133c
paulgraven <46284266+paulgraven@users.noreply.github.com>
2025-08-22 10:33:20 -0400

    Update database.py

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ DATABASE_URL = os.getenv("POSTGRES_URL") or os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise RuntimeError("Database URL environment variable is not set.")
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True, future=True)
 
 def get_engine():
     return engine
